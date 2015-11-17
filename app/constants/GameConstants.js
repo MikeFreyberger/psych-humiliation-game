28753902b28c246dde78b6776231c7d15e5bbdd1
MikeFreyberger <mdf3@princeton.edu>
2015-11-16 23:50:29 -0500

    Fix constants.

--- app/constants/GameConstants.js
+++ app/constants/GameConstants.js
@@ -3,9 +3,9 @@
  */
 module.exports = {
   game1: {
-    title: "GAME 1: Poke Dash to win!”,
+    title: "GAME 1: Poke Dash to win!",
     nextPage: 'game2',
-    alertMessage: 'Great job! That was just practice. The next round will be faster. Get ready.’,
+    alertMessage: 'Great job! That was just practice. The next round will be faster. Get ready.',
     clickCount: 5,
     getMessage: function() {
 
@@ -16,7 +16,7 @@ module.exports = {
     gameTitle: "game1"
   },
   game2: {
-    title: "GAME 2: Poke Dash 15 times to win!”,
+    title: "GAME 2: Poke Dash 15 times to win!",
     nextPage: 'game3',
     alertMessage1: "You are worse than 50% of Princeton students. ",
     alertMessage2: "You are better than 50% of Princeton students. ",
@@ -25,12 +25,12 @@ module.exports = {
       return Math.random() > .5 ? this.alertMessage1 : this.alertMessage2;
     },
     nextLinkText: "Next Game",
-    interval: 1000,
+    interval: 650,
     gameTitle: "game2"
 
   },
   game3: {
-    title: "GAME 3: Try again! Poke Dash 15 times to win.”,
+    title: "GAME 3: Try again! Poke Dash 15 times to win.",
     nextPage: 'survey',
     alertMessage: "Great job. Please continue on to a quick survey",
     clickCount: 15,
@@ -38,7 +38,7 @@ module.exports = {
       return this.alertMessage;
     },
     nextLinkText: "Survey",
-    interval: 800,
+    interval: 650,
     gameTitle: "game3"
   }
 };
