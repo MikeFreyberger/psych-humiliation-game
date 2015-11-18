a27db0f535123d2443419ed8b8064302c167e1a8
MikeFreyberger <mdf3@princeton.edu>
2015-11-18 12:04:13 -0500

    New title and update clickCount.

--- app/constants/GameConstants.js
+++ app/constants/GameConstants.js
@@ -3,7 +3,7 @@
  */
 module.exports = {
   game1: {
-    title: "GAME 1: Poke Dash to win!",
+    title: "GAME 1: Poke Dash to win! Go as fast as you can!",
     nextPage: 'game2',
     alertMessage: 'Great job! That was just practice. The next round will be faster. Get ready.',
     clickCount: 5,
@@ -16,11 +16,11 @@ module.exports = {
     gameTitle: "game1"
   },
   game2: {
-    title: "GAME 2: Poke Dash 15 times to win!",
+    title: "GAME 2: Poke Dash 25 times to win! Go as fast as you can!",
     nextPage: 'game3',
     alertMessage1: "You are worse than " + (47 + Math.floor(Math.random()*7)) + "% of Princeton students. ",
     alertMessage2: "You are better than " + (47 + Math.floor(Math.random()*7)) +"% of Princeton students. ",
-    clickCount: 15,
+    clickCount: 25,
     getMessage: function() {
       this.alertMessage1 = "You are worse than " + (47 + Math.floor(Math.random()*7)) + "% of Princeton students.";
       this.alertMessage2 = "You are better than " + (47 + Math.floor(Math.random()*7)) +"% of Princeton students.";
@@ -32,10 +32,10 @@ module.exports = {
 
   },
   game3: {
-    title: "GAME 3: Try again! Poke Dash 15 times to win.",
+    title: "GAME 3: Try again! Poke Dash 25 times to win! Go as fast as yoy can!",
     nextPage: 'survey',
     alertMessage: "Great job. Please continue on to a quick survey",
-    clickCount: 15,
+    clickCount: 25,
     getMessage: function() {
       return this.alertMessage;
     },
